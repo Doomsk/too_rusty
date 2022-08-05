1b3cf1745e51c44803d0666947c3e863e420318f
Kaonan Micadei <k.micadei@gmail.com>
2022-08-05 22:21:34 +0200

    make the list iterable

--- examples/linked_list/src/list.rs
+++ examples/linked_list/src/list.rs
@@ -17,7 +17,7 @@ impl<T> List<T> {
     fn push(&mut self, elem: T) {
         let new = Box::new(Node {
             elem,
-            next: self.head.take()  // same as `mem::replace(&mut self.head, None)`
+            next: self.head.take()  // `.take()`is the same as `mem::replace(&mut self.head, None)`
         });
 
         self.head = Some(new);
@@ -32,6 +32,20 @@ impl<T> List<T> {
     }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+    fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -40,20 +54,30 @@ mod tests {
     #[test]
     fn basics() {
         let mut list = List::new();
-        list.push(1);
-        list.push(2);
-        list.push(3);
+        list.push(1); list.push(2); list.push(3);
 
         assert_eq!(list.pop(), Some(3));
         assert_eq!(list.pop(), Some(2));
 
-        list.push(4);
-        list.push(5);
+        list.push(4); list.push(5);
         
         assert_eq!(list.pop(), Some(5));
         assert_eq!(list.pop(), Some(4));
 
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+        assert_eq!(iter.next(), None);
     }
 }
